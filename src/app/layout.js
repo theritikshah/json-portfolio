0a2b3e66a956d1ed74d2345944552589f76e029f
Anmol Agrawal <anmol.ag53@gmail.com>
2024-03-04 01:24:28 +0530

    Added metadata generation in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,19 @@
+import { promises as fs } from "fs";
+
+export const generateMetadata = async () => {
+  const file = await fs.readFile(
+    process.cwd() + "/src/app/data/profile.json",
+    "utf8"
+  );
+  const data = JSON.parse(file);
+  let metadata = {
+    ...data.meta,
+    applicationName: "JSON Portfolio",
+    authors: [{ name: "Anmol Agrawal", url: "https://anmolagrawal.dev" }],
+  };
+  return metadata;
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
