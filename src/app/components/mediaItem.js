305eb5bc047e0f71fd35608ec52a49009032b46e
Anmol Agrawal <anmol.ag53@gmail.com>
2024-04-15 03:05:14 +0530

    Added support for video media

--- src/app/components/mediaItem.js
+++ src/app/components/mediaItem.js
@@ -34,11 +34,22 @@ export default function MediaItem({ data }) {
       case "image":
         return (
           <div>
-            <img src={data.mediaDetails.src} alt="" />
+            <img width="100%" src={data.mediaDetails.src} alt="" />
           </div>
         );
       case "video":
-        return <></>;
+        return (
+          <video
+            width="100%"
+            autoPlay={data.mediaDetails.autoplay || true}
+            controls={data.mediaDetails.controls || false}
+            loop={data.mediaDetails.loop || true}
+            muted={data.mediaDetails.muted || true}
+          >
+            <source src={data.mediaDetails.src} type="video/mp4"></source>
+            Your browser does not support HTML video.
+          </video>
+        );
       case "image-slide":
         return <Carousel name={data.name} images={data.mediaDetails.src} />;
     }
