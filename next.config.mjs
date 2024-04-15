0065b8479445e172d5ee1e59672f1d4f2f31317a
Anmol Agrawal <anmol.ag53@gmail.com>
2024-04-15 14:19:05 +0530

    Reverted removal of automatically injection of basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/json-portfolio",
-  output: "export",
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
